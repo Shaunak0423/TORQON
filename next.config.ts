544a1f26a8b49fc5a12754ecde47d4938cda6176
Shaunak <shetshaunak23@gmail.com>
2026-07-22 19:41:46 +0530

    change in next config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-
 const nextConfig: NextConfig = {
-  ...(isGithubPages && {
-    output: "export",
-    basePath: "/TORQON",
-    assetPrefix: "/TORQON/",
-    images: { unoptimized: true },
-  }),
+  output: "export",
+  basePath: process.env.GITHUB_ACTIONS ? "/TORQON" : "",
+  assetPrefix: process.env.GITHUB_ACTIONS ? "/TORQON/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
