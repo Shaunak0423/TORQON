587e09e003dd6b4ca77bd7b876f4b0f92bf15941
Shaunak <shetshaunak23@gmail.com>
2026-07-22 19:39:07 +0530

    git hub page build changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.DEPLOY_TARGET === "gh-pages";
+const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
   ...(isGithubPages && {
