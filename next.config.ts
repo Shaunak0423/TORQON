5987991b786a8345c05a84b476c4ce9a66d1d186
Shaunak <shetshaunak23@gmail.com>
2026-07-22 20:06:07 +0530

    config change for image rendering

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/TORQON",
-  assetPrefix: "/TORQON",
+  assetPrefix: "/TORQON/",
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
-
